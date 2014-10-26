7d6c2ca19d1cb0c75828ceba0b6d74858121fd80
Nikivasil <nikivasil21@gmail.com>
2014-10-26 16:21:03 +0300

    Destroy/edit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,37 @@
 class CardsController < ApplicationController
   def index
-    @cards = Card.all
+   @cards = Card.all
   end
-  
+
   def show
     @card = Card.find(params[:id])  
   end
 
   def new
   end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+  
+  def create
+    @card = Card.new
+
+    if @card.save
+       redirect_to @card
+  end
+
+  def update
+    @card = Card.new(params[:id])
+
+    @card.update
+      redirect_to cards_path
+  end
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    
+    redirect_to cards_path
+  end   
+end
 end   
