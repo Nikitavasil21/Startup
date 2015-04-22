f5d7d30721cb14467267f938932ede05d9e4c883
Nikivasil <nikivasil21@gmail.com>
2015-04-22 18:05:30 +0300

    Nokogiri and Parsing words

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 require 'nokogiri'
 require 'open-uri'
 doc = Nokogiri::HTML(open("http://blogs.transparent.com/danish/2013/10/31/100-most-common-written-words-in-danish/"))
-
+#open Url
 doc.css('div > p + table tr')[1..100].each do |x|
   Card.create(original: x.css('td')[1].text.strip, translated: x.css('td')[2].text.strip)
 end
+#parsing words
