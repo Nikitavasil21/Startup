270eb8121e545a0a7e2384df44172b918fef41bf
Nikivasil <nikivasil21@gmail.com>
2015-04-25 12:12:33 +0300

    The hardest trainer in the world

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,20 @@
 class HomeController < ApplicationController
   def index
-    @card = Card.task_cards
+    @card = Card.task_cards.first
   end
 
   def task
-    user_translation = params[:user_translation]
-    if @card.task_checking
-      flash[:notice] = "Fuck yeah!"
+    @card = Card.find(params_task[:id])
+    if @card.task_checking(params_task[:user_translation])
+      flash.now[:success] = "Fuck yeah!"
     else
-      flash[:error] = "Try again,bro!"
+      flash.now[:error] = "Try again,bro!"
     end
     redirect_to root_path
   end
+
+  private
+    def params_task
+      params.permit(:id,:user_translation)
+    end
 end  
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,14 @@
 class Card < ActiveRecord::Base
-
+validates :original, :translated, :review_date, presence: true
 
   scope :task_cards, -> {
     where("review_date <=?", DateTime.now).order("RANDOM()")
   }
 
-  def self.task_checking(user_translation)
-    original.mb_chars.downcase == user_translation.mb_chars.downcase
+  def task_checking(user_translation)
+    if original.mb_chars.downcase == user_translation.mb_chars.downcase
+      update_attributes(review_date: Date.today + 3.days)
+    end
   end  
-
- 
+  
 end
