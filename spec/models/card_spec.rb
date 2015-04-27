bd0d00c6ee2fff151218a4282d38a3343d8c107f
Nikivasil <nikivasil21@gmail.com>
2015-04-27 11:58:21 +0300

    Delete HOogie Code and create a new test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -5,12 +5,18 @@ describe Card do
     card = Card.create(original: "cherry", translated: "Вишня", review_date: "2015-04-10 00:00:00 UTC")
     expect(card.check_translation("CheRrY")).to be true
   end
-  it "Check the simple example" do
+  it "Checking the simple example" do
     card = Card.create(original: "Hoogie code", translated: "Говнокод", review_date: "2015-04-10 00:00:00 UTC" )
     expect(card.check_translation("Hoogie code")).to be true
   end 
-  it "Check the bad translation" do
+  it "Returns false for incorrect translation" do
     card = Card.create(original: "Hoogie code", translated: "Говнокод", review_date: "2015-04-10 00:00:00 UTC")
     expect(card.check_translation("Shit")).to be false
   end
+
+  it "Cheking the changing of review_date" do
+    card = Card.create(original: "Hoogie code", translated: "Говнокод", review_date: "2015-04-27 00:00:00 UTC")
+    expect(card.check_translation("Hoogie code")).to be true
+    expect(card.review_date).to eq("2015-04-30 00:00:00 UTC")
+  end 
 end
