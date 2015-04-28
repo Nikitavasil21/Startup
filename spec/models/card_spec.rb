72971d9e9acb849a8f2f1bf6b4a099d687326531
Nikivasil <nikivasil21@gmail.com>
2015-04-28 13:22:26 +0300

    Delete 1 needless test and repaired syntax

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,14 +1,11 @@
 require "rails_helper"
 
 describe Card do
-  it "Check translation of the card" do 
+  it "Check translation of the card in the different register" do 
     card = Card.create(original: "cherry", translated: "Вишня", review_date: "2015-04-10 00:00:00 UTC")
     expect(card.check_translation("CheRrY")).to be true
   end
-  it "Checking the simple example" do
-    card = Card.create(original: "Hoogie code", translated: "Говнокод", review_date: "2015-04-10 00:00:00 UTC" )
-    expect(card.check_translation("Hoogie code")).to be true
-  end 
+
   it "Returns false for incorrect translation" do
     card = Card.create(original: "Hoogie code", translated: "Говнокод", review_date: "2015-04-10 00:00:00 UTC")
     expect(card.check_translation("Shit")).to be false
@@ -21,7 +18,7 @@ describe Card do
     end
   
     it "Cheking the changing of review_date" do
-      expect { card.check_translation"Hoogie code" }.to change { card.review_date }.from("2015-04-28 00:00:00 UTC").to("2015-05-1 00:00:00 UTC")
+      expect { card.check_translation("Hoogie code") }.to change { card.review_date }.from("2015-04-28 00:00:00 UTC").to("2015-05-1 00:00:00 UTC")
     end
   end 
 end
