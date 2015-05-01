1a3b9c027aa0e3354a3b7dd7527c567fbd5183a9
Nikivasil <nikivasil21@gmail.com>
2015-05-01 22:23:18 +0300

    Ignore tmp and add spaces

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Check the verify of the form" do
 
-let!(:card) { FactoryGirl.create (:card) }
+  let!(:card) { FactoryGirl.create (:card) }
 
   before do
     card.update_attribute(:review_date, Date.today - 1.day)
