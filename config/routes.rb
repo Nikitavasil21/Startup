753063b3152a65ae67ebff08e946bf51ae01030d
Nikivasil <nikivasil21@gmail.com>
2015-05-13 21:12:24 +0300

    9TH

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Startup::Application.routes.draw do
 
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+
   resources :profile, only: [:edit, :update]  
 
   resources :user_sessions
