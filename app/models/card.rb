822d7bd6b2a705bc33dd5116230e0781b7d56a75
Nikivasil <nikivasil21@gmail.com>
2015-05-16 19:44:00 +0300

    Added patrial and repair hound

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :picture, styles: { medium: "360x360"}
+  has_attached_file :picture, styles: { medium: "360x360" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
   validates :original, :translated, :review_date, :user_id, presence: true
   scope :for_review, -> {
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,15 +21,7 @@ Startup::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_host_name: 's3-us-west-1.amazonaws.com',
-    s3_credentials: {
-      bucket: ENV['S3_BUCKET_NAME'],
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-  }
-}
+  
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
