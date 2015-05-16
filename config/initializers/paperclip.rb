fc0bc01ea372f5e99b344009d4ab0f0aeeb83e91
Nikivasil <nikivasil21@gmail.com>
2015-05-16 23:03:38 +0300

    Added configure

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,7 @@
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_credentials] = {
-  bucket: ENV['S3_BUCKET_NAME'],
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  bucket: Figaro.env.s3_bucket_name,
+  access_key_id: Figaro.env.aws_access_key_id,
+  secret_access_key: Figaro.env.aws_secret_access_key
  }
