649797ebc8baf22b88c3acf35520fffee9158d85
Nikivasil <nikivasil21@gmail.com>
2015-05-23 23:26:40 +0300

    Added end

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -7,6 +7,7 @@ class DecksController < ApplicationController
     if @deck = current_user.decks.new(deck_params)
     @deck.save
       redirect_to decks_path, notice: "Successfully created"
+    end
   end
 
   private
